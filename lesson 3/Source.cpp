2a10f6e8986d4fb85d2718f6c518faae7f034fb9
Don master <Pruss>
2026-03-15 09:54:21 +0200

    Додав інформацію про базовий проект C++

---
+++ lesson 3/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<Windows.h>
+
+int main()
+{
+	using namespace std;
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+
+	cout << "Привіт! Гарний і чудовий ранок :)";
+
+	return 0;
+}
